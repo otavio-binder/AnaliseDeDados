dfa819e9c2bc8103e1ef2d5b8764465168505aee
otavio-binder <oabinder@gmail.com>
2023-10-22 18:20:28 -0300

    Adicionando bibliotecas

---
+++ projetoEstatistico/BlibsAndVersion.py
@@ -0,0 +1,16 @@
+#importando blibiotecas para a análise estátistica
+import numpy as np
+import pandas as pd 
+import scipy as scip
+import statsmodels as stmds
+#importando blibiotecas para a plotagem de gráficos
+import matplotlib as matplt
+import seaborn as seab
+
+
+print('Versão do pandas -> %s' % pd.__version__)
+print('Versão do numpy -> %s' % np.__version__)
+print('Versão do scipy -> %s' % scip.__version__)
+print('Versão do statsmodels -> %s' % stmds.__version__)
+print('Versão do seaborn -> %s' % seab.__version__)
+print('Versão do matplotlib -> %s' % matplt.__version__)
